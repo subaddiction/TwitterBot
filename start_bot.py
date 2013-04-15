291886643c1b5a287c164d41b375320cbcb64dc3
Kalen Wessel <kalen.wessel@gmail.com>
2013-04-15 15:47:40 -0700

    Bring up 10 results from query. Follow users with said hashtag

--- start_bot.py
+++ start_bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import twitter
+import sys
 from ConfigParser import SafeConfigParser
 from sets import Set
  
@@ -17,9 +18,13 @@ access_token_secret = parser.get('API_KEYS', 'access_token_secret')
 # Authenticate with twitter to use the Twitter API
 api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token_key=access_token, access_token_secret=access_token_secret)
 
-# Query for tweets with the hashtag rogue
-query = api.GetSearch("#rogue")
+
+# Query for tweets with the hashtag provided by the user
+query = api.GetSearch("#" + str(sys.argv[1]), per_page=10)
 
 # Loop through results and print out usernames
+print "Now following:"
 for tweet in query:
-	print tweet.user.screen_name
+	user = tweet.user.screen_name
+	print user
+	api.CreateFriendship(user)
