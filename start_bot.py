5c7c879ba37d5366a1dfbe3d3ec70f6d28160a0d
Kalen Wessel <kalen.wessel@gmail.com>
2013-04-15 21:45:56 -0700

    Creates list of found users. Removes duplicates. Shows you how many followers and friends you have.

--- start_bot.py
+++ start_bot.py
@@ -4,6 +4,27 @@ import twitter
 import sys
 from ConfigParser import SafeConfigParser
 from sets import Set
+
+# Sorting function to remove all duplicate users when searching for hashtags.
+# In some cases the same user may have tweeted multiple times in a row
+# with the same hashtag. This will create duplicate names. So by running the 
+# list of matched tweets through this function, duplicates can be removed.
+def removeDuplicates(seq, idfun=None): 
+   # order preserving
+   if idfun is None:
+       def idfun(x): return x
+   seen = {}
+   result = []
+   for item in seq:
+       marker = idfun(item)
+       # in old Python versions:
+       # if seen.has_key(marker)
+       # but in new ones:
+       if marker in seen: continue
+       seen[marker] = 1
+       result.append(item)
+   return result
+
  
 # Tell the parser which ini file to read
 parser = SafeConfigParser()
@@ -20,11 +41,21 @@ api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret, ac
 
 
 # Query for tweets with the hashtag provided by the user
-query = api.GetSearch("#" + str(sys.argv[1]), per_page=10)
+
+query = api.GetSearch("#" + str(sys.argv[1]), per_page=1000)
 
 # Loop through results and print out usernames
+
+twitterBot = api.GetUser("eurokidd")
+
+print "Followers: " + str(twitterBot.followers_count)
+print "Following: " + str(twitterBot.friends_count)
 print "Now following:"
+L = []
+index = 0
 for tweet in query:
 	user = tweet.user.screen_name
-	print user
+	L.insert(index+1, user)
 	api.CreateFriendship(user)
+ 
+print '\n'.join(removeDuplicates(L))
