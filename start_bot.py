9ed4a061a26685f660cbe38e5c048f748993680c
Kalen Wessel <kalen.wessel@gmail.com>
2013-04-19 22:05:41 -0700

    Added argument parsing for hashtag, adding of users, and removing of users. Deletion is currently commented out.

--- start_bot.py
+++ start_bot.py
@@ -2,6 +2,7 @@
 
 import twitter
 import sys
+import argparse
 from ConfigParser import SafeConfigParser
 from sets import Set
 
@@ -25,7 +26,6 @@ def removeDuplicates(seq, idfun=None):
        result.append(item)
    return result
 
- 
 # Tell the parser which ini file to read
 parser = SafeConfigParser()
 parser.read('api-keys.ini')
@@ -36,26 +36,41 @@ consumer_secret     = parser.get('API_KEYS', 'consumer_secret')
 access_token        = parser.get('API_KEYS', 'access_token')
 access_token_secret = parser.get('API_KEYS', 'access_token_secret')
 
+# Command-line argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("-hashtag", help="Unfollow users", action='store', type=str)
+parser.add_argument("-add", help="Follow users", action='store_true')
+parser.add_argument("-delete", help="Unfollow users", action='store_true')
+args = parser.parse_args()
+
 # Authenticate with twitter to use the Twitter API
 api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token_key=access_token, access_token_secret=access_token_secret)
 
-
 # Query for tweets with the hashtag provided by the user
+query = api.GetSearch("#" + str(args.hashtag), per_page=50)
 
-query = api.GetSearch("#" + str(sys.argv[1]), per_page=1000)
+if args.add:
+	print args.add
+	# Create a list for all the users found using the specified hashtag.
+	# Interate through the list and create friendships (Follow them)
+	L = []
+	index = 0
+	for tweet in query:
+		user = tweet.user.screen_name
+		L.insert(index+1, user)
+	#	api.CreateFriendship(user)
 
-# Loop through results and print out usernames
+	# Remove duplicates for clarity
+	print '\n'.join(removeDuplicates(L))
+	print "Followers: " + str(twitterBot.followers_count)
+	print "Following: " + str(twitterBot.friends_count)
+	print "Now following:"
 
-twitterBot = api.GetUser("eurokidd")
+if args.delete:
+	# Loop through friends and unfollow the 20 most recent followings.
+	#currentFriends = api.GetFriends()
+	#for f in currentFriends[:100]:
+	#	print f.screen_name
+	#	removeUser = api.DestroyFriendship(f.screen_name)
 
-print "Followers: " + str(twitterBot.followers_count)
-print "Following: " + str(twitterBot.friends_count)
-print "Now following:"
-L = []
-index = 0
-for tweet in query:
-	user = tweet.user.screen_name
-	L.insert(index+1, user)
-	api.CreateFriendship(user)
- 
-print '\n'.join(removeDuplicates(L))
+
